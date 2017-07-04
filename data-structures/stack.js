196ffbc79eace8968d791130285e6e5de02d1d50
Anton Bowden <bowden.anton@gmail.com>
2017-07-04 14:31:20 +0100

    Implement stack

---
+++ data-structures/stack.js
@@ -0,0 +1,33 @@
+function Stack(capacity) {
+  this.capacity = capacity || Infinity;
+  this.storage = {};
+  this.count = 0;
+}
+
+Stack.prototype.push = function(value) {
+  if (this.count < this.capacity) {
+    this.storage[this.count] = value;
+    this.count++;
+  } else {
+    return "Stack is full!";
+  }
+};
+
+Stack.prototype.pop = function() {
+  var value = this.storage[this.count];
+  delete this.storage[this.count];
+  this.count--;
+  return value;
+};
+
+Stack.prototype.peek = function() {
+  if (this.count === 0) {
+    return "Stack is empty!";
+  } else {
+    return this.storage[this.count - 1];
+  }
+};
+
+Stack.prototype.size = function() {
+  return this.count;
+};
