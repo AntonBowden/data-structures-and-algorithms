359373d1f717a4e53bdc8b42ad2f2839fc258585
Anton Bowden <bowden.anton@gmail.com>
2017-07-04 17:54:01 +0100

    Implement queue

---
+++ data-structures/queue.js
@@ -0,0 +1,33 @@
+function Queue(capacity) {
+  this.capacity = capacity || Infinity;
+  this.storage = {};
+  this.head = 0;
+  this.tail = 0;
+}
+
+Queue.prototype.enqueue = function(value) {
+  if (this.count() < this.capacity) {
+    this.storage[this.tail] = value;
+    this.tail++;
+    return this.count();
+  } else {
+    return 'Queue is full!';
+  }
+};
+
+Queue.prototype.dequeue = function() {
+  var value = this.storage[this.head];
+  delete this.storage[this.head];
+  if (this.head < this.tail) {
+    this.head++;
+  }
+  return value;
+};
+
+Queue.prototype.peek = function() {
+  return this.storage[this.head];
+};
+
+Queue.prototype.count = function() {
+  return this.tail - this.head;
+};
