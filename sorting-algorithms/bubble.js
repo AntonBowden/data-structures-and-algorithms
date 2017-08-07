17c238eeac7d78b625a0ed92d813681f2a3d54b6
Anton Bowden <bowden.anton@gmail.com>
2017-08-07 20:37:36 +0100

    implement bubble sort

---
+++ sorting-algorithms/bubble.js
@@ -0,0 +1,12 @@
+function bubbleSort(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 1; j < arr.length; j++) {      
+      if (arr[j] < arr[j-1]) {
+        var temp = arr[j];
+        arr[j] = arr[j-1];
+        arr[j-1] = temp;
+      }
+    }
+  }
+  return arr;
+};
