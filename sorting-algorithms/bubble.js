acd7c1357c4b111390cee04c39c983e60cb4da83
Anton Bowden <bowden.anton@gmail.com>
2017-08-08 03:04:36 +0100

    modify algorithm to exit early if array is partially sorted

--- sorting-algorithms/bubble.js
+++ sorting-algorithms/bubble.js
@@ -1,12 +1,15 @@
 function bubbleSort(arr) {
   for (var i = 0; i < arr.length; i++) {
-    for (var j = 1; j < arr.length; j++) {      
+    var swapped = false;
+    for (var j = 1; j < arr.length; j++) {
       if (arr[j] < arr[j-1]) {
         var temp = arr[j];
         arr[j] = arr[j-1];
         arr[j-1] = temp;
+        swapped = true;
       }
     }
+    if (!swapped) return arr;
   }
   return arr;
 };
